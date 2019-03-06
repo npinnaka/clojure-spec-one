c3c8f39028a3938bc873cac5e594490dddef6396
Narendra Pinnaka <npinnaka@yahoo.com>
2019-03-06 03:36:51 -0600

    added new files

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.logging "0.4.1"]]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/data.csv "0.1.4"]]
   :main ^:skip-aot clojure-spec-one.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_spec_one/core.clj
+++ src/clojure_spec_one/core.clj
@@ -1,14 +1,56 @@
 (ns clojure-spec-one.core
   (:require [clojure-spec-one.spec-validator :as sv]
-            [clojure-spec-one.Person :refer [create-person]])
+            [clojure.string :as strg]
+            [clojure-spec-one.Person :refer [create-person]]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
   (:gen-class))
 
-(def rahul {:first-name "Rahul" :last-name "Dravid" :gender "M" :favorite-color "white" })
+(def my-collection [{:first-name "Rahul" :last-name "Dravid" :gender "M" :middle-name nil}
+                    {:first-name "Sachin" :last-name "Tendulkar" :gender "M" :middle-name "Ramesh"}
+                    {:first-name "Rohit" :last-name "Sharma" :gender "M" :middle-name nil}
+                    {:first-name "Virat" :last-name "Kohli" :gender "M" :middle-name nil}
+                    {:first-name "Sania" :last-name "Mirza" :gender "F" :middle-name nil}])
 
-(def sachin {:first-name "Sachin" :last-name "Tendulkar" :middle-name "Ramesh" :gender "M" :favorite-color "black" })
 
-(def rahul-upper {:first-name "Rahul" :last-name "Dravid" :gender "m" :favorite-color "WHITE" })
+(defn wirte-file [collection file-name]
+  (spit file-name
+        (str (strg/join ", " (map name (keys (first my-collection)))) "\n"))
+  (map
+   #(spit file-name
+     (str
+      (->>
+       (vals %)
+       (strg/join ","))
+      "\n")
+     :append true)
+   my-collection))
 
-(def sachin-upper {:first-name "Sachin" :last-name "Tendulkar" :middle-name "Ramesh" :gender "M" :favorite-color "black" })
 
-(sv/valid-person? rahul)
+(wirte-file my-collection "resources/test.csv")
+
+(defn read-csv [file-name]
+  (with-open [reader (io/reader file-name)]
+    (doall
+     (csv/read-csv reader))))
+
+(defn write-csv[file-name csv-data]
+  (with-open [writer (io/writer file-name)]
+    (csv/write-csv writer
+                   csv-data)))
+
+(write-csv "resources/newfile.csv"
+           (concat (vector (into [] (map name (keys (first my-collection)))))
+                   (mapv
+                    #(->>
+                      (vals %))
+                    my-collection)))
+
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+       (rest csv-data)))
+
+(vec (csv-data->maps (read-csv (io/reader "resources/newfile.csv"))))
